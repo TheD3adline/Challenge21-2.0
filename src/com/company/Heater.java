329893ca60d71d7a0778205e66256ba583a844fb
MichaelFessler-Desktop <michaelfessler@gmx.at>
2022-04-14 10:07:08 +0200

    tidied Heater.changeLevel()

--- src/com/company/Heater.java
+++ src/com/company/Heater.java
@@ -20,8 +20,9 @@ public class Heater {
         } else if(this.level > 5) {
             this.level = 5;
             throw new TooHighException("5 is the highest possible setting of this heater! Heater set to level " + this.level);
+        } else {
+            System.out.println("Heating level successfully changed to " + this.level);
         }
-        System.out.println("Heating level successfully changed to " + this.level);
     }
 
     public int getLevel() {
